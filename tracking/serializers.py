040b93b28711f8fa2a55a6b05d1aceb5a2bb1767
chandusanjith <chandusanjith.t.2610@gmail.com>
2024-08-11 17:10:04 +0530

    updates

--- tracking/serializers.py
+++ tracking/serializers.py
@@ -5,16 +5,20 @@ from traces.models import Device, LeopardTraces
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
-        fields = ['device_id', 'device_name', 'last_active_on']
+        fields = ['device_id', 'device_name']
 
 class DeviceHealthCheckSerializer(serializers.Serializer):
     device_id = serializers.CharField(max_length=100)
-    created_at = serializers.DateTimeField()
+    last_active_on = serializers.DateTimeField()
+    cpu_usage = serializers.CharField(max_length=100)
+    memory_usage = serializers.CharField(max_length=100)
+    disk_usage = serializers.CharField(max_length=100)
+    temperature = serializers.CharField(max_length=100)
 
 class LeopardTracesSerializer(serializers.ModelSerializer):
     class Meta:
         model = LeopardTraces
-        fields = ['lat', 'long', 'place', 'description', 'device', 'traced_on']
+        fields = ['type','lat', 'long', 'area_code', 'confidence', 'device', 'traced_on']
 
     def validate_device(self, value):
         if not Device.objects.filter(id=value.id).exists():
