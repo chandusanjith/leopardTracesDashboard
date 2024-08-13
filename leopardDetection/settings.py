2233a8822f96d9fc370bc8a1726c734a687e81ae
Chandu Sanjith <chandusanjith.t.2610@gmail.com>
2024-08-13 12:09:03 +0530

    Update settings.py

--- leopardDetection/settings.py
+++ leopardDetection/settings.py
@@ -138,3 +138,4 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CSRF_TRUSTED_ORIGINS = ['*', 'https://leopardtracesdashboard-production.up.railway.app']
